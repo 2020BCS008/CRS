c3e5d7ab03451cb8e4932f229770cf3f62dbced5
Chaitanya R. Talware <96913187+2020BCS008@users.noreply.github.com>
2024-03-19 15:25:19 +0530

    Add files via upload

---
+++ CRS-Backend/src/main/java/com/crs/ComplaintRedressalSystem.java
@@ -0,0 +1,16 @@
+package com.crs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ComplaintRedressalSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintRedressalSystem.class, args);
+	}
+	
+
+}
